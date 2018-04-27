226b7fc5cc63cf691dd819f12c478db1525bc2eb
Jess <zjess987@gmail.com>
2018-04-27 13:17:32 -0400

    added player + player movement + camera controller
    camera follows player movement

---
+++ Game Jam/Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float Xmax;
+    public float Xmin;
+
+    public Vector3 offset;
+    public Transform player;
+
+    public float lerpSpeed;
+    
+
+    void Update()
+    {
+        Vector3 target = player.transform.position + offset;
+
+        /*float camXExtent = Camera.main.orthographicSize * Camera.main.aspect;
+
+
+        if (target.x + camXExtent > Xmax + camXExtent)
+        {
+            target.x = Xmax;
+        }
+
+        if (target.x - camXExtent < Xmin - camXExtent)
+        {
+            target.x = Xmin;
+        }
+        */
+
+        transform.position += (target - transform.position) * lerpSpeed;
+    }
+}
---
+++ Game Jam/Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool lastKeyLeft;
+    GameObject npc;
+
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+    public KeyCode interactKey;
+    
+    Vector2 moveDirection = Vector2.zero;
+    public float speed;
+
+    Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	void Update () {
+
+        moveDirection *= 0.75f;
+
+
+        if (Input.GetKey(rightKey))
+        {
+            moveDirection += Vector2.right;
+            Debug.Log("move right");
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKey(leftKey))
+        {
+            moveDirection += Vector2.left;
+            Debug.Log("move left");
+            spriteRenderer.flipX = true;
+        }
+        else if (lastKeyLeft)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveDirection.x * speed * Time.deltaTime, rb.velocity.y);
+    }
+}
